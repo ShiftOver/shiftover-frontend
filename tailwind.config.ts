df31200e83ea5a2cd48a599f73a16cea96fb622e
Win-TS <winthanatpong@gmail.com>
2024-08-31 23:18:06 +0700

    chore: setup eslint, prettier, husky, lint-staged

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,28 +1,28 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    colors: {
-      'shiftover-black': '#353535',
-      'shiftover-white-1': '#f7faf5',
-      'shiftover-white-2': '#f5f2f2',
-      'shiftover-blue': '#1fa9c4',
-      'shiftover-gray': '#484848',
-      'shiftover-teal': '#5deecc',
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        colors: {
+            'shiftover-black': '#353535',
+            'shiftover-white-1': '#f7faf5',
+            'shiftover-white-2': '#f5f2f2',
+            'shiftover-blue': '#1fa9c4',
+            'shiftover-gray': '#484848',
+            'shiftover-teal': '#5deecc',
+        },
+        extend: {
+            backgroundImage: {
+                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+                'gradient-conic':
+                    'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+            },
+        },
     },
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+    plugins: [],
 };
 export default config;
